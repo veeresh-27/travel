519d81ccc32a268454f9d39dcfb40cf63903810e
Veeresh <veereshrm789@gmail.com>
2022-02-21 15:49:56 +0530

    Co-authored-by: VARUGU DIVAKAR REDDY <DIVAKAR-REDDY-VARUGU@users.noreply.github.com>

--- pages/components/Layout.js
+++ pages/components/Layout.js
@@ -1,20 +1,16 @@
 import React from 'react'
+import Ticket from './ticket'
+import Usercards from './Usercards'
 
 function Layout() {
   return (
     <div className="container flex flex-wrap m-5 justify-center w-full">
-    <button class="bg-stone-200  hover:bg-neutral-400 text-black font-bold py-2 px-4 rounded m-5 w-2/5 h-40 text-4xl ">
-        <Link></Link>
-  </button>
-  <button class="bg-stone-200 hover:bg-neutral-400 text-black font-bold py-2 px-4 rounded m-5 w-2/5 h-40 text-4xl">
-    Search for bus
-  </button>
-  <button class="bg-stone-200  hover:bg-neutral-400 text-black font-bold py-2 px-4 rounded m-5 w-2/5 h-40 text-4xl">
-    Report an issue
-  </button>
-  <button class="bg-stone-200  hover:bg-neutral-400 text-black font-bold py-2 px-4 rounded m-5 w-2/5 h-40 text-4xl">
-    Get a Smart Card
-  </button>
+        <Usercards title="Get a Ticket" goto="component/ticket" icon={<svg xmlns="http://www.w3.org/2000/svg" class=" w-16 mx-5 text-orange-500"fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 5v2m0 4v2m0 4v2M5 5a2 2 0 00-2 2v3a2 2 0 110 4v3a2 2 0 002 2h14a2 2 0 002-2v-3a2 2 0 110-4V7a2 2 0 00-2-2H5z" />
+    </svg>}/>
+        <Usercards title="Search For a Bus" goto="#"/>
+        <Usercards title="Report an Issue" goto="#"/>
+        <Usercards title="Get a Smart Card" goto="#"/>
     </div>
   )
 }
---
+++ pages/components/Usercards.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from 'next/link'
+
+function Usercards({title,goto,icon}) {
+  return (
+    <button class="bg-stone-200  hover:bg-neutral-400 text-black font-bold py-2 px-4 rounded m-5 w-2/5 h-40 text-4xl flex items-center  justify-center">
+    <Link href={goto}>
+    <a>{icon}
+        {title}</a>
+  </Link>
+</button>
+  )
+}
+
+export default Usercards
