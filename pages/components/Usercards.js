59c8ec33f3b013e07bb4b40346b0e4fa60cd8a11
Veeresh R M <veereshrm789@gmail.com>
2023-06-21 11:07:23 +0530

    Update Usercards.js

--- pages/components/Usercards.js
+++ pages/components/Usercards.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 function Usercards({title,goto,icon}) {
   return (
-    <button class="bg-stone-200  hover:bg-neutral-400 text-black font-bold py-2 px-4 rounded m-5 w-2/5 h-40 text-4xl flex items-center  justify-center">
+    <button className="bg-stone-200  hover:bg-neutral-400 text-black font-bold py-2 px-4 rounded m-5 w-2/5 h-40 text-4xl flex items-center  justify-center">
     <Link href={goto}>
     <a>{icon}
         {title}</a>
